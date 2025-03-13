93d83f289ebfd872ac02a55721a1de48e36c07a3
brucekomike <brucekomike@gmail.com>
2025-03-13 15:38:05 +0800

    minor fix

--- source/conf.py
+++ source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'Makefile',
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'fumo'
+html_theme = 'furo'
 html_static_path = ['_static']
 html_last_updated_fmt = ''
 html_css_files = [
